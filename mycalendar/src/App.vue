aca5d3d126a0d6860c88f8f81e822b63ae02fa85
OlivierZoungrana <synthejazz@gmail.com>
2021-11-17 12:24:05 +0100

    modification de la date

--- mycalendar/src/App.vue
+++ mycalendar/src/App.vue
@@ -1,8 +1,60 @@
 <template>
   <v-app>
+  <div>
+    <v-app-bar
+      color="deep-purple accent-4"
+      dense
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Mon agenda</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+  </div>
+
+  <div class="vide">
+
+  </div>
+<v-container>
     <v-main>
       <router-view/>
     </v-main>
+</v-container>
   </v-app>
 </template>
 
@@ -16,3 +68,11 @@ export default {
   }),
 };
 </script>
+
+
+<style scoped>
+
+.vide{
+  padding: 2em;
+}
+</style>
